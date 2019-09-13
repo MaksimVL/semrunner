44d30cd42de94efa775718d0a070519ed6d69350
odrinkwa <odrinkwa@student.21-school.ru>
2019-09-13 23:55:31 +0300

    work on get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,38 +1,72 @@
+
+
 #include "get_next_line.h"
 
-int 	crash_f(char **str, char **buff)
+static int	crash_f(char **str, char **line, char **buff)
 {
-	if (*str)
-		free(*str);
-	*str = NULL;
-	if (*buff)
-		free(*buff);
-	*buff = NULL;
+	ft_memdel((void**)str);
+	ft_memdel((void**)line);
+	ft_memdel((void**)buff);
 	return (-1);
+}
+
+char 	*remake_str(char **str, char *new_point)
+{
+	char	*ptr;
+	if (!(ptr = ft_strdup(new_point + 1)))
+		return (NULL);
+	ft_memdel((void**)str);
+	*str = ptr;
+	return (ptr);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	static	t_charbuff	*str;
+	static char			*str;
 	char 				*buff;
 	int					read_count;
 	char 				*chr_n;
 
-	if (str)
-
 	if (!(buff = (char*)malloc(sizeof(char) * BUFF_SIZE)))
-		return (crash_f(&str, &buff));
+		return (-1);
+	*line = NULL;
+	if (str != NULL)
+	{
+		if ((chr_n = ft_strnstr(str, "\n", ft_strlen(str))))
+		{
+			if (!(*line = ft_strnjoin(line, str, (size_t)(chr_n - str))))
+				return (crash_f(&str, NULL, NULL));
+			if (chr_n == &str[ft_strlen(str) - 1])
+				ft_memdel((void**)&str);
+			else
+				if (!(str = remake_str(&str ,chr_n)))
+					return (crash_f(NULL, line, NULL));
+			return (1);
+		}
+		else
+		{
+			if (!(*line = ft_strnjoin(line, str, ft_strlen(str))))
+				return (crash_f(NULL, line, NULL));
+			ft_memdel((void**)&str);
+		}
+	}
 	while ((read_count = read(fd, buff, BUFF_SIZE)))
 	{
 		if ((chr_n = ft_strnstr(buff, "\n", read_count)))
-			*line = (char*)ft_rememalloc(sizeof(char) * ft_min());
+		{
 
-
+			if (!(*line = ft_strnjoin(line, buff, (int)(chr_n - buff))))
+				return (crash_f(&str, line, &buff));
+			if (read_count > (size_t)(chr_n - buff))
+			if (!(str = ft_strnjoin(&str, chr_n + 1, read_count - (size_t)(chr_n - str + 1))))
+			return (1);
+		}
+		else
+			if (!(*line = ft_strnjoin(line, buff, read_count)))
+				return (crash_f(&str, line, &buff));
 	}
 	if (read_count < 0)
-		return (crash_f(&str, &buff));
-
-
-
-	return 0;
+		return (crash_f(&str, line, &buff));
+	ft_memdel((void**)&buff);
+	return (0);
 }
