c79826c97645a5b22b0b73c6e1aa086d4542039b
odrinkwa <odrinkwa@student.21-school.ru>
2019-10-05 03:38:40 +0300

    hz? how include mlx in CLion CMakefile ??

---
+++ fdf/libft/sources/ft_memalloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odrinkwa <odrinkwa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/09 20:47:29 by odrinkwa          #+#    #+#             */
+/*   Updated: 2019/09/26 13:18:25 by semenbegunov     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void	*ptr;
+
+	if (!(ptr = (void*)malloc(size)))
+		return (NULL);
+	ft_memset(ptr, 0, size);
+	return (ptr);
+}
--- fdf/main.c
+++ fdf/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <unistd.h>
+
+#include "mlx.h"
+#include "libft.h"
+
+int deal_key(int key, void *param)
+{
+	if (param)
+	{}
+	printf("%d", key);
+	return (0);
+}
 
 int main()
 {
-	printf("Hello, World!\n");
+	void *mlx_ptr;
+	void *win_ptr;
+	char *str = "test";
+
+	printf("test");
+	ft_putchar('a');
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 640, 480, str);
+//	mlx_key_hook(win_ptr, deal_key, (void *)0);
+//	mlx_loop(mlx_ptr);
 	return 0;
 }
+
