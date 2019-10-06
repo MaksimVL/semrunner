c32196aac5ea24e119d9a0bc64584122c15b2b58
Sem Begunov <smbeg@mail.ru>
2019-10-06 18:10:52 +0300

    fdf - test make

--- fdf/main.c
+++ fdf/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#include "mlx.h"
+#include "./mlx/mlx.h"
 #include "libft.h"
 
 int deal_key(int key, void *param)
@@ -18,12 +18,12 @@ int main()
 	void *win_ptr;
 	char *str = "test";
 
-	printf("test");
+	printf("tes3434aat");
 	ft_putchar('a');
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 640, 480, str);
-//	mlx_key_hook(win_ptr, deal_key, (void *)0);
-//	mlx_loop(mlx_ptr);
+	mlx_key_hook(win_ptr, deal_key, (void *)0);
+	mlx_loop(mlx_ptr);
 	return 0;
 }
 
